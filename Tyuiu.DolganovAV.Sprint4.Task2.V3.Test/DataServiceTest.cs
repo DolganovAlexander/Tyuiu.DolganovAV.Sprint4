1c1cbefdb74fb11f905ed71f1c49effd079365ec
Dolganov Alexander <Tyuiu.DolganovAV@outlook.com>
2025-10-17 22:00:55 +0500

    Task2

---
+++ Tyuiu.DolganovAV.Sprint4.Task2.V3.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.DolganovAV.Sprint4.Task2.V3.Lib;
+namespace Tyuiu.DolganovAV.Sprint4.Task2.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[] array = { 2, 4, 5, 2, 7, 4, 5, 6, 3, 4, 2, 5 };
+            Assert.AreEqual(24, ds.Calculate(array));
+        }
+    }
+}
---
+++ Tyuiu.DolganovAV.Sprint4.Task2.V3/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.DolganovAV.Sprint4.Task2.V3.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        DataService ds = new DataService();
+        Console.Title = "Спринт #4 | Выполнил: Долганов А. В. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерные массивы (генератор случайных чисел)                    *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #3                                                              *");
+        Console.WriteLine("* Выполнил: Долганов Александр Витальевич | ПИНб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая подсчитывает сумму четных элементов массива *");
+        Console.WriteLine("* , который заполнен случаными числами от 2 до 7                          *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int len;
+        Console.Write("Введите количество элементов массива: ");
+        len = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[len];
+
+        for (int i = 0; i < len; i++)
+        {
+            array[i] = rnd.Next(2, 7);
+        }
+
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < len; i++)
+        {
+            Console.Write(array[i] + "\t");
+        }
+
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int res = ds.Calculate(array);
+        Console.WriteLine($"Сумма четных элементов = {res}");
+        Console.ReadKey();
+    }
+}
