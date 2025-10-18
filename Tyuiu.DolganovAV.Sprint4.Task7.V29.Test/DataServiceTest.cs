8dcc14cb81558acd16505fbfc2e1bab35ac812ce
Dolganov Alexander <Tyuiu.DolganovAV@outlook.com>
2025-10-19 01:58:20 +0500

    Task7

---
+++ Tyuiu.DolganovAV.Sprint4.Task7.V29.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.DolganovAV.Sprint4.Task7.V29.Lib;
+namespace Tyuiu.DolganovAV.Sprint4.Task7.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            int n = 4;
+            int m = 3;
+            string value = "983157642891";
+            Assert.AreEqual(28, ds.Calculate(n, m, value));
+        }
+    }
+}
--- Tyuiu.DolganovAV.Sprint4.Task7.V29/Program.cs
+++ Tyuiu.DolganovAV.Sprint4.Task7.V29/Program.cs
@@ -1,4 +1,4 @@
-﻿using Tyuiu.DolganovAV.Sprint4.Task5.V5.Lib;
+﻿using Tyuiu.DolganovAV.Sprint4.Task7.V29.Lib;
 internal class Program
 {
     private static void Main(string[] args)
@@ -7,32 +7,42 @@ internal class Program
         Console.Title = "Спринт #4 | Выполнил: Долганов А. В. | ПИНб-25-1";
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* Спринт #4                                                               *");
-        Console.WriteLine("* Тема: Класс Array                                                       *");
-        Console.WriteLine("* Задание #6                                                              *");
-        Console.WriteLine("* Вариант #5                                                              *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #29                                                             *");
         Console.WriteLine("* Выполнил: Долганов Александр Витальевич | ПИНб-25-1                     *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* УСЛОВИЕ:                                                                *");
-        Console.WriteLine("* Написать программу, которая подсчитывает количество элементов массива,  *");
-        Console.WriteLine("* длина которых равна 5                                                   *");
+        Console.WriteLine("* Написать программу, которая преобразовывает строку в матрицу 4 на 3 и   *");
+        Console.WriteLine("* подсчитывает сумму четных чисел                                         *");
         Console.WriteLine("*                                                                         *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
 
-        var week = new string[] { "Меркурий", "Венера", "Земля", "Марс", "Юпитер", "Сатурн", "Уран", "Нептун" };
+        int n = 4;
+        int m = 3;
+        string value = "983157642891";
+        int[,] matrix = new int[n, m];
 
-        for (int i = 0;  i < week.Length; i++)
+        int index = 0;
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < n; i++)
         {
-            Console.WriteLine(week[i]);
+            for (int j = 0; j < m; j++)
+            {
+                Console.Write($"{value[index]}\t");
+                index++;
+            }
+            Console.WriteLine();
         }
 
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
         Console.WriteLine("***************************************************************************");
 
-        var res = ds.Calculate(week);
-        Console.WriteLine($"Количество элементов, длина которых равна 5: {res}");
+        int res = ds.Calculate(n, m, value);
+        Console.WriteLine($"Сумма четных элементов массива равна: {res}");
         Console.ReadLine();
     }
 }
